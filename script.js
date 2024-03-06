cc70b7c5d449c12c83b78e5940fe2f83048e4c13
ashish vaidya <ashishvaidya683@gmail.com>
2024-03-06 21:25:38 +0530

    third Commit

--- script.js
+++ script.js
@@ -37,3 +37,21 @@ var swiper = new Swiper(".mySwiper", {
       prevEl: ".swiper-button-prev",
     },
   });
+
+  // number counting
+  let valueDisplays = document.querySelectorAll(".num");
+  let interval = 4000;
+
+  valueDisplays.forEach((valueDisplay) => {
+    let startValue = 0;
+    let endValue = parseInt(valueDisplay.getAttribute("data-val"));
+    let duration = math.floor(interval / endValue);
+    let counter = setInterval(function () {
+      startValue += 1;
+      valueDisplay.textContent = startValue;
+
+      if (startValue == endValue) {
+        clearInterval(counter);
+      }
+    }, duration);
+  });
